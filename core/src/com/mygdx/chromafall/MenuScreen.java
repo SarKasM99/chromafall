b3703ebc2810cd440156ffc9f1be4037ddfc14d0
Charles Packo <charles.packo@student.uclouvain.be>
2022-04-17 10:16:52 +0000

    Comments + structure + buttons resizing

--- core/src/com/mygdx/chromafall/MenuScreen.java
+++ core/src/com/mygdx/chromafall/MenuScreen.java
@@ -40,7 +40,6 @@ public class MenuScreen implements Screen {
 
     private final int w = Gdx.graphics.getWidth();
     private final int h = Gdx.graphics.getHeight();
-    private SpriteBatch batch;
     protected Stage stage;
 
     private MyGdxGame game;
@@ -55,65 +54,62 @@ public class MenuScreen implements Screen {
 
     public MenuScreen(MyGdxGame gameArg) {
         screen = this;
-        game = gameArg;
+        game = gameArg;    // MyGdxGame instance
 
-        batch = new SpriteBatch();
-
-        stage = new Stage(new ExtendViewport(w, h));
+        stage = new Stage(new ExtendViewport(w, h));    // Stage handles the viewport and distributes input events.
     }
 
 
     @Override
     public void show() {
-        //Stage should control input
+        // Stage should control input
         Gdx.input.setInputProcessor(stage);
 
-        //Create Table
+        // Creates Table (for the buttons locations)
         mainTable = new Table();
         optionTable = new Table();
 
-        //Set table to fill stage*
+        // Sets table to fill stage
         mainTable.setFillParent(true);
         optionTable.setFillParent(true);
 
-        //Set alignment of contents in the table.
+        // Sets alignment of contents in the table. (table on the top)
         mainTable.top();
         optionTable.top();
 
-        //Creat font
-        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("fonts/myFont.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        // Creates font
+        FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("fonts/myFont.ttf"));    // Font generator with model myFont.ttf
+        FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();    // Parameters of the font
         param.size = 70;
         param.color = Color.WHITE;
         //param.borderWidth = 50;
 
-        //button case
-        TextureRegionDrawable button = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("EmptyButton.png"))));
-        ImageTextButton.ImageTextButtonStyle buttonstyle = new ImageTextButton.ImageTextButtonStyle();
-        buttonstyle.font = gen.generateFont(param);
-        buttonstyle.up = button;
+        // Button case
+        ImageTextButton.ImageTextButtonStyle buttonStyle = new ImageTextButton.ImageTextButtonStyle();    // Parameters (style) of the button
+        buttonStyle.font = gen.generateFont(param);    // Generates the font with parameters param
+        buttonStyle.up = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("EmptyButton.png"))));    // Image of the button
 
-        //Create buttons for the main table
-        ImageTextButton playButton = new ImageTextButton("Play",buttonstyle);
-        ImageTextButton optionsButton = new ImageTextButton("Options",buttonstyle);
-        ImageTextButton quitButton = new ImageTextButton("Quit",buttonstyle);
+        // Creates buttons for the main table
+        ImageTextButton playButton = new ImageTextButton("Play",buttonStyle);
+        ImageTextButton optionsButton = new ImageTextButton("Options",buttonStyle);
+        ImageTextButton quitButton = new ImageTextButton("Quit",buttonStyle);
 
-        //Create buttons for the options table
-        final ImageTextButton soundButton = new ImageTextButton("Sound : On",buttonstyle);
-        final ImageTextButton musicButton = new ImageTextButton("Music : On",buttonstyle);
-        ImageTextButton backButton  = new ImageTextButton("Back",buttonstyle);
+        // Creates buttons for the options table
+        final ImageTextButton soundButton = new ImageTextButton("Sound : On",buttonStyle);
+        final ImageTextButton musicButton = new ImageTextButton("Music : On",buttonStyle);
+        ImageTextButton backButton  = new ImageTextButton("Back",buttonStyle);
 
-        //logo
+        // Logo
         Image logoMain = new Image(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("chroma-fall-logo.png")))));
         Image logoOption = new Image(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("chroma-fall-logo.png")))));
 
-        //Add listeners to buttons of the main table
+        // Adds listeners to buttons of the main table
         playButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 mainTable.reset();
-                if(game.isSoundOn()) open.play();
-                if(game.isMusicOn()) menuMusic.pause();
+                if(game.isSoundOn()) open.play();    // Opening sound
+                if(game.isMusicOn()) menuMusic.pause();    // Menu music stops
                 game.setScreen(new GameScreen(game, screen));
             }
         });
@@ -123,19 +119,19 @@ public class MenuScreen implements Screen {
             public void clicked(InputEvent event, float x, float y) {
                 if(game.isSoundOn()) open.play();
                 mainTable.remove();
-                stage.addActor(optionTable);
+                stage.addActor(optionTable);    // Replaces the main table
             }
         });
 
         quitButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                if(game.isSoundOn()) close.play();
+                if(game.isSoundOn()) close.play();    // Closing sound
                 Gdx.app.exit();
             }
         });
 
-        //Add listeners to buttons of the option table
+        // Adds listeners to buttons of the option table
         soundButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -176,29 +172,34 @@ public class MenuScreen implements Screen {
             public void clicked(InputEvent event, float x, float y) {
                 if(game.isSoundOn()) close.play();
                 optionTable.remove();
-                stage.addActor(mainTable);
+                stage.addActor(mainTable);    // Replaces the option table
             }
         });
 
-        //Add buttons to main table
-        mainTable.defaults().width(0.85f* w);
-        mainTable.defaults().height(0.10f* h);
-        mainTable.defaults().pad(0.01f* h);
+        // Adds buttons to MainTable
+
+        // Default cells options for the table
+        mainTable.defaults().width(0.60f * w);
+        mainTable.defaults().height(0.10f * h);
+        mainTable.defaults().pad(0.01f * h);    // Space between cells
         mainTable.defaults().align(Align.center);
-        mainTable.add(logoMain).size(0.45f*h).padBottom(0);
-        mainTable.row();
+
+        // Fills the table with the buttons (and logo)
+        mainTable.add(logoMain).size(0.40f*h).padBottom(0);
+        mainTable.row();    // Next cell
         mainTable.add(playButton);
         mainTable.row();
         mainTable.add(optionsButton);
         mainTable.row();
         mainTable.add(quitButton);
 
-        //Add buttons to optionTable
-        optionTable.defaults().width(0.85f* w);
-        optionTable.defaults().height(0.10f* h);
-        optionTable.defaults().pad(0.01f* h);
+        // Adds buttons to optionTable
+        optionTable.defaults().width(0.60f * w);
+        optionTable.defaults().height(0.10f * h);
+        optionTable.defaults().pad(0.01f * h);
         optionTable.defaults().align(Align.center);
-        optionTable.add(logoOption).size(0.45f*h).padBottom(0);
+
+        optionTable.add(logoOption).size(0.40f*h).padBottom(0);
         optionTable.row();
         optionTable.add(soundButton);
         optionTable.row();
@@ -206,9 +207,10 @@ public class MenuScreen implements Screen {
         optionTable.row();
         optionTable.add(backButton);
 
-        //Add table to stage
+        // Adds MainTable to stage first
         stage.addActor(mainTable);
 
+        // Sounds and music
         open = Gdx.audio.newSound(Gdx.files.internal("Sounds/open.wav"));
         close = Gdx.audio.newSound(Gdx.files.internal("Sounds/close.wav"));
         menuMusic = Gdx.audio.newMusic(Gdx.files.internal("Sounds/menu_music.wav"));
@@ -216,13 +218,14 @@ public class MenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        stage.getViewport().apply();
-        Gdx.gl.glClearColor(.102f,.102f,.102f, 1);
+        stage.getViewport().apply();    // Applies the viewport to the camera
+        Gdx.gl.glClearColor(.102f,.102f,.102f, 1);    // Background
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act();
         stage.draw();
 
+        // Menu music looping
         if(game.isMusicOn()) {
             menuMusic.setLooping(true);
             menuMusic.play();
