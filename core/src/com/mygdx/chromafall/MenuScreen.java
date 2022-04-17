e0f0a04f29069375ae9304258fa0064ffafd0e5a
Charles Packo <charles.packo@student.uclouvain.be>
2022-04-17 13:43:53 +0000

    The deathScreen is now here

--- core/src/com/mygdx/chromafall/MenuScreen.java
+++ core/src/com/mygdx/chromafall/MenuScreen.java
@@ -42,8 +42,10 @@ public class MenuScreen implements Screen {
     private final int h = Gdx.graphics.getHeight();
     protected Stage stage;
 
+    private int score;
+
     private MyGdxGame game;
-    private Screen screen;
+    private MenuScreen screen;
 
     private Music menuMusic;
     private Sound open;
@@ -51,14 +53,24 @@ public class MenuScreen implements Screen {
 
     private Table mainTable;
     private Table optionTable;
+    private Table deathTable;
 
     public MenuScreen(MyGdxGame gameArg) {
         screen = this;
         game = gameArg;    // MyGdxGame instance
+        score = 0;
 
         stage = new Stage(new ExtendViewport(w, h));    // Stage handles the viewport and distributes input events.
+
+        // Sounds and music
+        open = Gdx.audio.newSound(Gdx.files.internal("Sounds/open.wav"));
+        close = Gdx.audio.newSound(Gdx.files.internal("Sounds/close.wav"));
+        menuMusic = Gdx.audio.newMusic(Gdx.files.internal("Sounds/menu_music.wav"));
     }
 
+    public void setScore(int scoreObtained) {
+        score = scoreObtained;
+    }
 
     @Override
     public void show() {
@@ -68,21 +80,34 @@ public class MenuScreen implements Screen {
         // Creates Table (for the buttons locations)
         mainTable = new Table();
         optionTable = new Table();
+        deathTable = new Table();
 
         // Sets table to fill stage
         mainTable.setFillParent(true);
         optionTable.setFillParent(true);
+        deathTable.setFillParent(true);
 
         // Sets alignment of contents in the table. (table on the top)
         mainTable.top();
         optionTable.top();
+        deathTable.top();
 
-        // Creates font
+        // Creates fonts
+
+        // Buttons
         FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("fonts/myFont.ttf"));    // Font generator with model myFont.ttf
         FreeTypeFontGenerator.FreeTypeFontParameter param = new FreeTypeFontGenerator.FreeTypeFontParameter();    // Parameters of the font
         param.size = 70;
         param.color = Color.WHITE;
         //param.borderWidth = 50;
+
+        // Game over
+        FreeTypeFontGenerator fontGen = new FreeTypeFontGenerator(Gdx.files.internal("fonts/myFont.ttf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter fontParams = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        fontParams.size = 110;
+        fontParams.color = Color.WHITE;
+        fontParams.borderColor = Color.FIREBRICK;
+        fontParams.borderWidth = 8;
 
         // Button case
         ImageTextButton.ImageTextButtonStyle buttonStyle = new ImageTextButton.ImageTextButtonStyle();    // Parameters (style) of the button
@@ -95,9 +120,14 @@ public class MenuScreen implements Screen {
         ImageTextButton quitButton = new ImageTextButton("Quit",buttonStyle);
 
         // Creates buttons for the options table
-        final ImageTextButton soundButton = new ImageTextButton("Sound : On",buttonStyle);
-        final ImageTextButton musicButton = new ImageTextButton("Music : On",buttonStyle);
+        final ImageTextButton soundButton = new ImageTextButton("Sound : ON",buttonStyle);
+        final ImageTextButton musicButton = new ImageTextButton("Music : ON",buttonStyle);
         ImageTextButton backButton  = new ImageTextButton("Back",buttonStyle);
+
+        // Creates buttons for the death table
+        ImageTextButton playAgainButton = new ImageTextButton("Play again",buttonStyle);
+        ImageTextButton goBackButton = new ImageTextButton("Go back to menu",buttonStyle);
+        ImageTextButton quitDeathButton = new ImageTextButton("Quit",buttonStyle);
 
         // Logo
         Image logoMain = new Image(new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("chroma-fall-logo.png")))));
@@ -107,9 +137,9 @@ public class MenuScreen implements Screen {
         playButton.addListener(new ClickListener(){
             @Override
             public void clicked(InputEvent event, float x, float y) {
-                mainTable.reset();
                 if(game.isSoundOn()) open.play();    // Opening sound
                 if(game.isMusicOn()) menuMusic.pause();    // Menu music stops
+                mainTable.remove();
                 game.setScreen(new GameScreen(game, screen));
             }
         });
@@ -136,14 +166,14 @@ public class MenuScreen implements Screen {
             @Override
             public void clicked(InputEvent event, float x, float y) {
                 if(game.isSoundOn()) {
-                    soundButton.setText("Sound : off");
+                    soundButton.setText("Sound : OFF");
                     game.setSoundOn(false);
                 }
 
                 else{
                     game.setSoundOn(true);
                     open.play();
-                    soundButton.setText("Sound : on");
+                    soundButton.setText("Sound : ON");
                 }
             }
         });
@@ -154,13 +184,13 @@ public class MenuScreen implements Screen {
                 if(game.isSoundOn()) open.play();
 
                 if(game.isMusicOn()) {
-                    musicButton.setText("Music : off");
+                    musicButton.setText("Music : OFF");
                     game.setMusicOn(false);
                     menuMusic.pause();
                 }
 
                 else{
-                    musicButton.setText("Music : on");
+                    musicButton.setText("Music : ON");
                     game.setMusicOn(true);
                     menuMusic.play();
                 }
@@ -176,9 +206,37 @@ public class MenuScreen implements Screen {
             }
         });
 
+        // Adds listeners to buttons of the death table
+        playAgainButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(game.isSoundOn()) open.play();
+                deathTable.remove();
+                game.setScreen(new GameScreen(game, screen));
+            }
+        });
+
+        goBackButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(game.isSoundOn()) close.play();
+                if(game.isSoundOn()) menuMusic.play();
+                deathTable.remove();
+                stage.addActor(mainTable);
+            }
+        });
+
+        quitDeathButton.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                if(game.isSoundOn()) close.play();
+                Gdx.app.exit();
+            }
+        });
+
         // Adds buttons to MainTable
 
-        // Default cells options for the table
+        // Default cells options for the main table
         mainTable.defaults().width(0.60f * w);
         mainTable.defaults().height(0.10f * h);
         mainTable.defaults().pad(0.01f * h);    // Space between cells
@@ -207,13 +265,38 @@ public class MenuScreen implements Screen {
         optionTable.row();
         optionTable.add(backButton);
 
-        // Adds MainTable to stage first
-        stage.addActor(mainTable);
+        // Adds buttons to deathTable
+        deathTable.defaults().width(0.60f * w);
+        deathTable.defaults().height(0.10f * h);
+        deathTable.defaults().pad(0.01f * h);
+        deathTable.defaults().align(Align.center);
 
-        // Sounds and music
-        open = Gdx.audio.newSound(Gdx.files.internal("Sounds/open.wav"));
-        close = Gdx.audio.newSound(Gdx.files.internal("Sounds/close.wav"));
-        menuMusic = Gdx.audio.newMusic(Gdx.files.internal("Sounds/menu_music.wav"));
+        // Game Over
+        Label.LabelStyle titleLabelStyle = new Label.LabelStyle(fontGen.generateFont(fontParams), Color.WHITE);
+        Label gameOverTitle = new Label("Game Over!", titleLabelStyle);
+        gameOverTitle.setAlignment(Align.center);
+        fontGen.dispose();    // Freeing fontGen to avoid memory leaks
+
+        // Score
+        Label.LabelStyle labelStyle = new Label.LabelStyle(gen.generateFont(param), Color.WHITE);
+        Label scoreLabel = new Label("Score : " + score, labelStyle);
+        scoreLabel.setAlignment(Align.center);
+
+        deathTable.row();
+        deathTable.add(gameOverTitle).padTop(0.10f* h);
+        deathTable.row();
+        deathTable.add(scoreLabel).padBottom(0.15f* h);
+        deathTable.row();
+        deathTable.add(playAgainButton);
+        deathTable.row();
+        deathTable.add(goBackButton);
+        deathTable.row();
+        deathTable.add(quitDeathButton);
+
+        // Adds MainTable to stage first
+        if (score == 0) stage.addActor(mainTable);
+        // Adds deathTable if a game was played (score != 0)
+        else stage.addActor(deathTable);
     }
 
     @Override
